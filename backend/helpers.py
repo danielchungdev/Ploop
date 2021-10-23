8d0d085970ac5089ee619bd9056d5fb8c1c51674
mark-a-maluenda <themarkmaluenda@gmail.com>
2021-10-23 19:44:12 -0400

    Mark: helpers.py

--- backend/helpers.py
+++ backend/helpers.py
@@ -3,3 +3,13 @@ from haversine import Unit
 
 def find_distance(loc1, loc2):
   return hs.haversine(loc1, loc2, unit=Unit.METERS)
+
+# Gender Types
+MALE = 0
+FEMALE = 1
+OTHER = 2
+
+# Dry Method
+PAPERTOWEL = 0
+AIRDRY = 1
+NONE = 2
