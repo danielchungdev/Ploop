4dd5b1b52c09492ea66e766a3b1d927a364f7539
mark-a-maluenda <themarkmaluenda@gmail.com>
2021-10-23 00:44:53 -0400

    Mark: GET users, GET user, POST user endpoints

--- backend/database.py
+++ backend/database.py
@@ -10,28 +10,58 @@ dynamo_client = boto3.client("dynamodb")
 # User Methods
 # -------------------------------------------------
 
-"""
-Get All Users
-"""
 def get_users():
-    return dynamo_client.scan(
+    user = dynamo_client.scan(
         TableName=USER_DATABASE
     )
+    return user["Items"]
 
-def get_user():
+def get_user(username):
+    user = dynamo_client.get_item(
+        TableName=USER_DATABASE,
+        Key={
+            'username' : {
+                'S' : username
+            }
+        }
+    )
+    if "Item" in user:
+        return user["Item"]
+    else:
+        return None
+
+def add_user(username, password, gender, email):
+    user = {
+        'username' : { "S" : username },
+        'password' : { "S" : password },
+        'gender' : { "S" : gender },
+        'email' : { "S" : email }
+    }
+    dynamo_client.put_item(
+        TableName=USER_DATABASE,
+        Item=user
+    )
+    return {
+        'username' : username,
+        'password' : password,
+        'gender' : gender,
+        'email' : email
+    }
+
+def edit_user(username):
     pass
 
-def edit_user():
+def delete_user(username):
     pass
 
 # -------------------------------------------------
 # Bathroom Methods
 # -------------------------------------------------
 
-"""
-Get All Bathrooms
-"""
 def get_bathrooms():
     return dynamo_client.scan(
         TableName=BATHROOM_DATABASE
     )
+
+def get_bathroom(bathroom_id):
+    pass
--- backend/flaskapp.py
+++ backend/flaskapp.py
@@ -1,7 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, Response, jsonify, request
+from werkzeug.wrappers import response
 from database import ( 
 	get_bathrooms, 
-	get_users
+	get_users,
+	get_user,
+	add_user,
+	edit_user,
+	delete_user
 )
 
 app = Flask(__name__)
@@ -11,7 +16,31 @@ app = Flask(__name__)
 # -------------------------------------------------
 @app.route("/api/users", methods=['GET'])
 def get_users_route():
-	return jsonify(get_users()["Items"])
+	return jsonify(get_users())
+
+@app.route("/api/user/<string:username>", methods=['GET'])
+def get_user_route(username):
+	try:
+		user = get_user(username)
+	except Exception:
+		return "User Not Found", 404
+	return user
+
+@app.route("/api/user/<string:username>", methods=['POST'])
+def post_user_route(username):
+	request_data = request.get_json()
+	user_exists_check = get_user(username)
+	if user_exists_check != None:
+		return "User Already Exists", 404
+	try:
+		password = request_data["password"] 
+		email = request_data["email"]
+		gender = request_data["gender"]
+		added_user = add_user(username, password, gender, email)
+	except Exception:
+		return "Insufficient Request Body", 404
+	return added_user
+	
 
 # -------------------------------------------------
 # Bathroom Routes
