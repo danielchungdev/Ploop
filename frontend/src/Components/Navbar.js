9289a5243f46849d32c39ae482b1b80c1705bb46
landonbu <brandonbl2021@gmail.com>
2021-10-23 04:34:52 -0400

    nav bar

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -4,12 +4,38 @@ import logo from '../Assets/Ploop.svg'
 import * as FaIcons from "react-icons/fa"
 import * as AiIcons from "react-icons/ai"
 import {Link} from 'react-router-dom'
-import { SidebarData } from './SidebarData'
+
+import {Dropdown, Menu} from 'antd'
+
+
+const menu = (
+    <Menu>
+        <Menu.item key = '0'>
+<div>
+    hello
+</div>
+        </Menu.item>
+        <Menu.item key = '1'>
+<div>
+    hello
+</div>
+        </Menu.item>
+        <Menu.item key = '2'>
+<div>
+    hello
+</div>
+        </Menu.item>
+
+    </Menu>
+)
 
 export default function Navbar() {
     return (
         <div className="navbar--container">
             <img src={logo} alt="Ploop Logo"/>
+            <Dropdown overlay= {menu} trigger = {['click']}>
+                <button>Click me</button>
+            </Dropdown>
         </div>
     )
 }
