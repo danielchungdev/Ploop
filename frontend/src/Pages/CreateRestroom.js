e122332b1aafb4b52478b0d12b67907a02ffb0f1
Pikachungg <pikachungg@gmail.com>
2021-10-23 18:03:12 -0400

    asdfasdfasdf

--- frontend/src/Pages/CreateRestroom.js
+++ frontend/src/Pages/CreateRestroom.js
@@ -5,16 +5,27 @@ import Input from '../Components/Input'
 import '../Sass/main.scss'
 import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
-import { Slider } from '@mui/material'
+import { Slider, RadioGroup, FormControlLabel, Radio } from '@mui/material'
+import Button from '../Components/Button'
 
 export default function CreateRestroom() {
 
     const [cleanliness, setCleanliness] = useState(1);
     const [ambience, setAmbience] = useState(1);
     const [crowdiness, setCrowdiness] = useState(50)
+    const [dryingValue, setDryingValue] = useState(0)
+    const [babyStation, setBabyStation] = useState(0)
 
     const handleSlider = (event, newValue) => {
         setCrowdiness(newValue)
+    }
+
+    const handleDryingChange = (event) => {
+        setDryingValue(event.target.value)
+    }
+
+    const handleBabyChange = (event) => {
+        setBabyStation(event.target.value)
     }
 
     return (
@@ -52,9 +63,35 @@ export default function CreateRestroom() {
                     </div>
 
                     <div>
-                        
+                        <Typography component="legend">Drying Method</Typography>
+                        <RadioGroup row
+                            aria-label="Drying Method"
+                            defaultValue={dryingValue}
+                            name="radio-buttons-group"
+                            onChange={handleDryingChange}
+                        >
+                            <FormControlLabel value="0" control={<Radio sx={{color: "#7F5F20", '&.Mui-checked':{color: "#7F5F20"}}}/>} label="Paper" />
+                            <FormControlLabel value="1" control={<Radio sx={{color: "#7F5F20", '&.Mui-checked':{color: "#7F5F20"}}}/>} label="Dryer" />
+                            <FormControlLabel value="2" control={<Radio sx={{color: "#7F5F20", '&.Mui-checked':{color: "#7F5F20"}}}/>} label="None" />
+                        </RadioGroup>
+                    </div>
+
+                    <div>
+                        <Typography component="legend">Baby Station</Typography>
+                        <RadioGroup row
+                            aria-label="Drying Method"
+                            defaultValue={babyStation}
+                            name="radio-buttons-group"
+                            onChange={handleBabyChange}
+                        >
+                            <FormControlLabel value="0" control={<Radio sx={{color: "#7F5F20", '&.Mui-checked':{color: "#7F5F20"}}}/>} label="Yes" />
+                            <FormControlLabel value="1" control={<Radio sx={{color: "#7F5F20", '&.Mui-checked':{color: "#7F5F20"}}}/>} label="No" />
+                        </RadioGroup>
                     </div>
                 </form>
+                <div class="button-container">
+                    <Button>Create Bathroom</Button>
+                </div>
             </div>
         </div>
     )
