9b4615c2b8a5a20a1c5377d12d0dae49f7898e1f
mark-a-maluenda <themarkmaluenda@gmail.com>
2021-10-23 13:47:06 -0400

    Mark: Redoing Deployment Strategy

--- backend/app.py
+++ backend/app.py
@@ -63,4 +63,4 @@ def get_bathrooms_route():
 	return jsonify(get_bathrooms()["Items"])
 
 if __name__ == '__main__':
-	app.run('127.0.0.1', port=5000, debug=True)
+	app.run('0.0.0.0', port=5000, debug=True)
