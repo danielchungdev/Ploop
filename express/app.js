07efead1beb68768d2d93f1cd400fdebedebc878
Pikachungg <pikachungg@gmail.com>
2021-10-24 01:51:08 -0400

    asdfasdfasdf

--- express/app.js
+++ express/app.js
@@ -1,8 +1,8 @@
 
-const express = require('express')
-const app = express()
-const port = 5000
-var cors = require('cors')
+const express = require('express');
+const app = express();
+const port = 5000;
+var cors = require('cors');
 const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://pikachungg:Fufupapachon23@cluster0.nfqh2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -20,6 +20,44 @@ app.get('/', (req, res) => {
 })
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/signup', (req, res) => {
+    console.log("I'M CALLED")
+    // const { Username: userDB, Password: passwordDB, Email: emailDb, Gender: genderdb} = req.body;
+    client.connect( async (err) => {
+        const collection = client.db("Ploop").collection("Users");
+        let singleUser = await collection.findOne({Username: "markmaluenda"})
+        console.log(singleUser)
+        res.send(singleUser)
+    })
+    // res.send("HEHEHEHEHHE")
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
